435a6902bfd995b809f2041348daed68ac200761
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-02-11 13:41:02 +0100

    first step

---
+++ src/fr/nathan/videotheque/App.java
@@ -0,0 +1,11 @@
+package fr.nathan.videotheque;
+
+import fr.nathan.videotheque.services.VideothequeApp;
+
+public class App {
+
+	public static void main(String[] args) {
+		new VideothequeApp().run();
+	}
+
+}
---
+++ src/fr/nathan/videotheque/services/VideothequeApp.java
@@ -0,0 +1,10 @@
+package fr.nathan.videotheque.services;
+
+import fr.nathan.videotheque.utils.ConsoleManager;
+
+public class VideothequeApp {
+
+	public void run() {
+		ConsoleManager.getInstance().printToConsole("Hello world", true);
+	}
+}
